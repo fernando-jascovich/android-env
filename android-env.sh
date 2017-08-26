964e6ae0675b481270a57004849a966e276d353e
Fernando Jascovich <fernando.ej@gmail.com>
2017-08-25 22:56:30 -0300

    emulator, avd and source updates

--- android-env.sh
+++ android-env.sh
@@ -15,3 +15,27 @@ alias android-env-run='docker run \
        -v android-env-packages:/opt/android-sdk \
        android-env ./gradlew '
 
+alias android-emulator='~/.android/sdk/tools/emulator'
+
+function android-create-emulator {
+    echo "Creating device with android-$1 called: $2"
+    echo "Verifying required packages..."
+    sdkmanager emulator
+    sdkmanager platform-tools
+    sdkmanager "platforms;android-$1"
+    sdkmanager "system-images;android-$1;google_apis;x86"
+    export PATH=$PATH:~/.android/sdk/platform-tools
+    echo "Creating avd..."
+    avdmanager create avd -c 512M \
+	       -k "system-images;android-$1;google_apis;x86" \
+	       -n $2 \
+	       -b google_apis/x86 \
+	       -d "Nexus 5" \
+	       --force
+    echo "Setting up device..."
+    echo "hw.gpu.enabled=yes" >> $ANDROID_SDK_ROOT/../avd/$2.avd/config.ini
+    echo "skin.dynamic=no" >> $ANDROID_SDK_ROOT/../avd/$2.avd/config.ini
+    echo "skin.name=1080x1920" >> $ANDROID_SDK_ROOT/../avd/$2.avd/config.ini
+    echo "skin.path=1080x1920" >> $ANDROID_SDK_ROOT/../avd/$2.avd/config.ini
+    $ANDROID_SDK_ROOT/tools/mksdcard -l SDCARD 512M $ANDROID_SDK_ROOT/../avd/$2.avd/sdcard.img
+}
--- readme.md
+++ readme.md
@@ -24,21 +24,10 @@ curl -O https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
 unzip sdk-tools-linux-3859397.zip
 export PATH=$PATH:~/.android/sdk/tools
 export PATH=$PATH:~/.android/sdk/tools/bin
-export ANDROID_HOME ~/.android/sdk
-```
+export ANDROID_SDK_ROOT ~/.android/sdk
 
-### Host emulator creation
-```
-sdkmanager --update emulator
-avdmanager create avd -c 512M \
-	   -k "system-images;android-26;google_apis;x86" \
-	   -n android-26 \
-	   -b google_apis/x86 \
-	   -d "Nexus 5"
-echo "hw.gpu.enabled=yes" >> $ANDROID_SDK_ROOT/../avd/android-26.avd/config.ini
-echo "skin.dynamic=no" >> $ANDROID_SDK_ROOT/../avd/android-26.avd/config.ini
-echo "skin.name=1080x1920" >> $ANDROID_SDK_ROOT/../avd/android-26.avd/config.ini
-echo "skin.path=1080x1920" >> $ANDROID_SDK_ROOT/../avd/android-26.avd/config.ini
+# Bash aliases step is required for this
+android-create-emulator 26 phone-oreo
 ```
 
 ### Notes
